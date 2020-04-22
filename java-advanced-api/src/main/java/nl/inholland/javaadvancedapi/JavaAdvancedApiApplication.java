e6819028bbe99f4168677ced034dcca164d5c14d
KEVIN <kevin@qubics.nl>
2020-04-22 13:09:43 +0200

    java2lesson1

---
+++ java-advanced-api/src/main/java/nl/inholland/javaadvancedapi/JavaAdvancedApiApplication.java
@@ -0,0 +1,13 @@
+package nl.inholland.javaadvancedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdvancedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAdvancedApiApplication.class, args);
+	}
+
+}
